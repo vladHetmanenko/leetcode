1a1bf5afa176e4adbed818c9be17c5462efbbdd4
Hetmanenko <vlad_hetmanenko@sp-lutsk.com>
2025-03-03 23:18:00 +0200

    feat(): resolve task 9

---
+++ 9. Palindrome Number/main.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isPalindrome(self, x: str) -> bool:
+        """
+        :type x: int
+        :rtype: bool
+        """
+        return str(x) == str(x)[::-1]
+    
+# Test case 1
+# Input: 121
+# Output: true
+# Expected: true
+
+
+# Test case 2
+# Input: -121
+# Output: false
+# Expected: false
+
+
+# Test case 3
+# Input: 10
+# Output: false
+# Expected: false
